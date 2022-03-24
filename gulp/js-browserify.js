782ed0a18ff7e5717ee366ea264a46339591c9c1
ComradeVanti <kingvanti650@gmail.com>
2022-03-24 10:52:24 +0100

    Add logic to compile straight from ts

--- gulp/js-browserify.js
+++ gulp/js-browserify.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const jsBrowserify = require('browserify');
 const source = require('vinyl-source-stream');
+const tsify = require("tsify");
 
 module.exports = function (SOURCE_DIR, ENTRY_FILE_NAME, BUILD_DIR, OUT_FILE_NAME) {
     gulp.task('js-browserify', function () {
         return jsBrowserify({ entries: [SOURCE_DIR + '/' + ENTRY_FILE_NAME] })
+            .plugin(tsify)
             .bundle()
             .pipe(source(OUT_FILE_NAME))
             .pipe(gulp.dest(BUILD_DIR));
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const SOURCE_DIR = 'src';
 const BUILD_DIR = 'build';
 
-require('./gulp/js-browserify')(SOURCE_DIR, 'main.js', BUILD_DIR, 'bundle.js');
+require('./gulp/js-browserify')(SOURCE_DIR, 'main.ts', BUILD_DIR, 'bundle.js');
 require('./gulp/js-clean-src')(SOURCE_DIR);
