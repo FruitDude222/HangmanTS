c230dce6194a5f09a6ee379e438a592862f88465
ComradeVanti <kingvanti650@gmail.com>
2022-03-24 10:56:56 +0100

    Add minification logic

--- gulp/js-browserify.js
+++ gulp/js-browserify.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const jsBrowserify = require('browserify');
 const source = require('vinyl-source-stream');
 const tsify = require("tsify");
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
 
 module.exports = function (SOURCE_DIR, ENTRY_FILE_NAME, BUILD_DIR, OUT_FILE_NAME) {
     gulp.task('js-browserify', function () {
@@ -9,6 +11,8 @@ module.exports = function (SOURCE_DIR, ENTRY_FILE_NAME, BUILD_DIR, OUT_FILE_NAME
             .plugin(tsify)
             .bundle()
             .pipe(source(OUT_FILE_NAME))
+            .pipe(buffer())
+            .pipe(uglify())
             .pipe(gulp.dest(BUILD_DIR));
     });
 }
